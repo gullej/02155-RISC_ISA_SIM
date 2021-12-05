d6df2045b099a4152fd68d0b36724c3dac7f9e94
gullej <gustav@juul-jorgensen.com>
2021-12-05 17:22:25 +0100

    Final commit

--- src/IsaSim.java
+++ src/IsaSim.java
@@ -1,10 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class IsaSim {
 
@@ -87,6 +81,105 @@ public class IsaSim {
             pc += 4; // incease program counter
 
         }
+    }
+
+    private static int reg_imm_instr(int instr) {
+        int funct3 = (instr >> 12) & 0b111;
+        int rs1 = (instr >> 15) & 0x1f;
+        int imm = (instr >> 20);
+
+        switch (funct3) {
+            case 0b000: // add sub
+
+                return reg[rs1] + imm;
+            case 0b001: // sll
+                imm &= 0b11111;
+                return reg[rs1] << imm;
+            case 0b010: // slt
+                return reg[rs1] < imm ? 1 : 0;
+            case 0b011: // slt unsigned
+                return Integer.compareUnsigned(reg[rs1], imm) < 0 ? 1 : 0;
+            case 0b100: // xor
+                return reg[rs1] ^ imm;
+            case 0b101: // sr
+                int funct7 = instr >> 25;
+                imm &= 0b11111;
+                return funct7 > 0 ? reg[rs1] >> imm : reg[rs1] >>> imm;
+            case 0b110: // or
+                return reg[rs1] | imm;
+            case 0b111: // and
+                return reg[rs1] & imm;
+            default:
+                System.out.println("funct3 " + funct3 + " is not supported for opcode OP_IMM");
+                return 0;
+        }
+    }
+
+    private static int reg_reg_instr(int instr) {
+        int funct3 = (instr >> 12) & 0b111;
+        int rs1 = (instr >> 15) & 0x1f;
+        int rs2 = (instr >> 20) & 0b11111;
+
+        switch (funct3) {
+            case 0b000: // add sub
+                int funct7 = instr >> 25;
+                return funct7 > 0 ? reg[rs1] - reg[rs2] : reg[rs1] + reg[rs2];
+            case 0b001: // sll
+                return reg[rs1] << reg[rs2];
+            case 0b010: // slt
+                return reg[rs1] < reg[rs2] ? 1 : 0;
+            case 0b011: // slt unsigned
+                return Integer.compareUnsigned(reg[rs1], reg[rs2]) < 0 ? 1 : 0;
+            case 0b100: // xor
+                return reg[rs1] ^ reg[rs2];
+            case 0b101: // sr
+                funct7 = rs2 >> 25;
+                return funct7 > 0 ? reg[rs1] >> reg[rs2] : reg[rs1] >>> reg[rs2];
+            case 0b110: // or
+                return reg[rs1] | reg[rs2];
+            case 0b111: // and
+                return reg[rs1] & reg[rs2];
+            default:
+                System.out.println("funct3 " + funct3 + " is not supported for opcode OP_IMM");
+                return 0;
+        }
+    }
+
+    private static int branch_instruction(int instr) {
+        int funct3 = (instr >> 12) & 0b111;
+        int rs1 = (instr >> 15) & 0b11111;
+        int rs2 = (instr >> 20) & 0b11111;
+        int offset = calculate_offset(instr);
+
+        switch (funct3) {
+            case 0b000: // beq
+                return reg[rs1] == reg[rs2] ? offset : 4;
+            case 0b001: // bne
+                return reg[rs1] != reg[rs2] ? offset : 4;
+            case 0b100: // blt
+                return reg[rs1] < reg[rs2] ? offset : 4;
+            case 0b101: // bge
+                return reg[rs1] >= reg[rs2] ? offset : 4;
+            case 0b110: // bltu
+                return Integer.compareUnsigned(reg[rs1], reg[rs2]) < 0 ? offset : 4;
+            case 0b111: // bgeu
+                return Integer.compareUnsigned(reg[rs1], reg[rs2]) > 0 ? offset : 4;
+            default:
+                System.out.println("funct3 " + funct3 + " is not supported for opcode BRANCH");
+                return 0;
+        }
+    }
+
+    private static int calculate_offset(int instr) {
+        int offset_1 = (instr >>> 7) & 0b11111;
+        int offset_2 = (instr >>> 25) & 0b1111111;
+
+        int offset = (((offset_1 & 0b1) << 11 |
+                (offset_1 & 0b11110) |
+                (offset_2 & 0b111111) << 5 |
+                (offset_2 & 0b1000000) << 6) << 19) >> 19;
+
+        return offset;
     }
 
     private static int load_from_memory(int instr) {
@@ -175,133 +268,9 @@ public class IsaSim {
         }
     }
 
-    private static void print_registers() {
-        for (int i : reg) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-    }
-
-    private static void dump_registers() {
-        try (DataOutputStream outputStream = new DataOutputStream(new FileOutputStream("out.res"))) {
-            for (int value : reg) {
-                int buffer[] = new int[4];
-                buffer[3] = value >>> 24;
-                buffer[2] = (value >>> 16) & 0xff;
-                buffer[1] = (value >>> 8) & 0xff;
-                buffer[0] = value & 0xff;
-
-                for (int i = 0; i < 4; i++) {
-                    outputStream.writeByte(buffer[i]);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static int reg_imm_instr(int instr) {
-        int funct3 = (instr >> 12) & 0b111;
-        int rs1 = (instr >> 15) & 0x1f;
-        int imm = (instr >> 20);
-
-        switch (funct3) {
-            case 0b000: // add sub
-
-                return reg[rs1] + imm;
-            case 0b001: // sll
-                imm &= 0b11111;
-                return reg[rs1] << imm;
-            case 0b010: // slt
-                return reg[rs1] < imm ? 1 : 0;
-            case 0b011: // slt unsigned
-                return Integer.compareUnsigned(reg[rs1], imm) < 0 ? 1 : 0;
-            case 0b100: // xor
-                return reg[rs1] ^ imm;
-            case 0b101: // sr
-                int funct7 = instr >> 25;
-                imm &= 0b11111;
-                return funct7 > 0 ? reg[rs1] >> imm : reg[rs1] >>> imm;
-            case 0b110: // or
-                return reg[rs1] | imm;
-            case 0b111: // and
-                return reg[rs1] & imm;
-            default:
-                System.out.println("funct3 " + funct3 + " is not supported for opcode OP_IMM");
-                return 0;
-        }
-    }
-
-    private static int reg_reg_instr(int instr) {
-        int funct3 = (instr >> 12) & 0b111;
-        int rs1 = (instr >> 15) & 0x1f;
-        int rs2 = (instr >> 20) & 0b11111;
-
-        switch (funct3) {
-            case 0b000: // add sub
-                int funct7 = instr >> 25;
-                return funct7 > 0 ? reg[rs1] - reg[rs2] : reg[rs1] + reg[rs2];
-            case 0b001: // sll
-                return reg[rs1] << reg[rs2];
-            case 0b010: // slt
-                return reg[rs1] < reg[rs2] ? 1 : 0;
-            case 0b011: // slt unsigned
-                return Integer.compareUnsigned(reg[rs1], reg[rs2]) < 0 ? 1 : 0;
-            case 0b100: // xor
-                return reg[rs1] ^ reg[rs2];
-            case 0b101: // sr
-                funct7 = rs2 >> 25;
-                return funct7 > 0 ? reg[rs1] >> reg[rs2] : reg[rs1] >>> reg[rs2];
-            case 0b110: // or
-                return reg[rs1] | reg[rs2];
-            case 0b111: // and
-                return reg[rs1] & reg[rs2];
-            default:
-                System.out.println("funct3 " + funct3 + " is not supported for opcode OP_IMM");
-                return 0;
-        }
-    }
-
     private static int load_unsigned_i(int instr) {
         int imm = (instr >>> 12) << 12;
         return imm;
-    }
-
-    private static int branch_instruction(int instr) {
-        int funct3 = (instr >> 12) & 0b111;
-        int rs1 = (instr >> 15) & 0b11111;
-        int rs2 = (instr >> 20) & 0b11111;
-        int offset = calculate_offset(instr);
-
-        switch (funct3) {
-            case 0b000: // beq
-                return reg[rs1] == reg[rs2] ? offset : 4;
-            case 0b001: // bne
-                return reg[rs1] != reg[rs2] ? offset : 4;
-            case 0b100: // blt
-                return reg[rs1] < reg[rs2] ? offset : 4;
-            case 0b101: // bge
-                return reg[rs1] >= reg[rs2] ? offset : 4;
-            case 0b110: // bltu
-                return Integer.compareUnsigned(reg[rs1], reg[rs2]) < 0 ? offset : 4;
-            case 0b111: // bgeu
-                return Integer.compareUnsigned(reg[rs1], reg[rs2]) > 0 ? offset : 4;
-            default:
-                System.out.println("funct3 " + funct3 + " is not supported for opcode BRANCH");
-                return 0;
-        }
-    }
-
-    private static int calculate_offset(int instr) {
-        int offset_1 = (instr >>> 7) & 0b11111;
-        int offset_2 = (instr >>> 25) & 0b1111111;
-
-        int offset = (((offset_1 & 0b1) << 11 |
-                (offset_1 & 0b11110) |
-                (offset_2 & 0b111111) << 5 |
-                (offset_2 & 0b1000000) << 6) << 19) >> 19;
-
-        return offset;
     }
 
     private static int jump_and_link_reg(int instr) {
@@ -334,30 +303,29 @@ public class IsaSim {
         return offset;
     }
 
-    private static void print_regs_from_dump(String filename) {
-        List<Integer> read_reg = new ArrayList<>();
-
-        try (DataInputStream inputStream = new DataInputStream(
-                new FileInputStream(filename))) {
-            while (inputStream.available() > 0) {
-                int buffer[] = new int[4];
-                for (int i = 0; i < 4; i++) {
-                    buffer[i] = inputStream.readUnsignedByte();
-                }
-                int regs = ((buffer[3]) << 24 |
-                        (buffer[2]) << 16 |
-                        (buffer[1]) << 8 |
-                        (buffer[0]));
-                read_reg.add(regs);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        for (int i : read_reg) {
+    private static void print_registers() {
+        for (int i : reg) {
             System.out.print(i + " ");
         }
         System.out.println();
     }
+
+    private static void dump_registers() {
+        try (DataOutputStream outputStream = new DataOutputStream(new FileOutputStream("out.res"))) {
+            for (int value : reg) {
+                int buffer[] = new int[4];
+                buffer[3] = value >>> 24;
+                buffer[2] = (value >>> 16) & 0xff;
+                buffer[1] = (value >>> 8) & 0xff;
+                buffer[0] = value & 0xff;
+
+                for (int i = 0; i < 4; i++) {
+                    outputStream.writeByte(buffer[i]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
